9910f6f63d42783bf6397af0d69132ef3e98481f
ErnieBuyco <buycoernie@gmail.com>
2020-06-09 18:44:44 +0800

    todo_finale

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+        <div id="app">
+                <!-- <div id="nav">
+                    <router-link to="/">Home</router-link>
+                    <router-link to="/about">About</router-link>
+                </div> -->
+                <Header/>
+                <router-view/>
+        </div>
+</template>
+
+
+<script>
+import Header from './components/layout/Header';
+export default {
+    name: 'app',
+    components: {
+        Header
+    }
+}
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+
+    }
+
+    body{
+            font-family: Arial, Helvetica, sans-serif;
+            line-height: 1.4;
+    }
+
+    .btn {
+            display: inline-block;
+            border: none;
+            background: #555555;
+            color: #ffffff;
+            padding: 7px 20px;
+            cursor: pointer;
+    }
+
+    .btn:hover{
+            background: #666666;
+    }
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about">
+                    <h1>About</h1>
+                    <p>This is an app TodoList Vuerouter and Axios</p>
+    </div>
+</template>
